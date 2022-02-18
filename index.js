d0f2d86bf9a0915602a2fb2cddb2ee22eafbbfa9
pelakk <80285605+pelakk@users.noreply.github.com>
2022-02-18 20:08:31 +0100

    make it better

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
   // Website allowed to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "http://blog.dawid-matuszynski.pl/"
+  );
 
   // Request methods
   res.setHeader("Access-Control-Allow-Methods", "GET, POST");
