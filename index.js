3ef37e73ce82e6cb48bd2f02caf8a56cdc68625c
pelakk <80285605+pelakk@users.noreply.github.com>
2022-02-18 20:15:49 +0100

    make it better

--- index.js
+++ index.js
@@ -11,26 +11,6 @@ app.use(upload());
 
 app.use(bodyParser.json());
 
-app.use(function (req, res, next) {
-  // Website allowed to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://dawid-matuszynski.pl/");
-
-  // Request methods
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
-
-  // Request headers
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  next();
-});
-
 app.use("/images", express.static("uploads"));
 
 app.use("/posts", postsRoutes);
